49ed413dd0b70082a83750827a7f40a12c8012ce
kwongyumin <rbals7398@naver.com>
2024-06-11 22:50:29 +0900

    - 프로젝트 환경설정

---
+++ src/main/java/org/tech/prac_webflux/PracWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.tech.prac_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tech/prac_webflux/PracWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tech.prac_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
